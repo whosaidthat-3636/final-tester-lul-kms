9ad58cc94d97f61d273c969b90d92e8a014ac4a6
whosaidthat-3636 <jong788@aucklanduni.ac.nz>
2025-02-16 23:30:47 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,40 @@
 let button;
-let buttonPressed = 0; // 0 = normal, 1 = button pressed
+let buttonPressed = 0;
 
-let textX = 210;
-let textY = 100;
+let textX = 0.25; // Percentage of windowWidth
+let textY = 0.1;  // Percentage of windowHeight
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   button = createButton('Oh you found me');
-  button.position(280, 320);
-  button.hide(); // Start hidden
+  button.position(windowWidth * 0.4, windowHeight * 0.4); // Position as a percentage of window size
+  button.hide(); 
   button.mousePressed(() => {
-    buttonPressed = 1; // Switch to new page
+    buttonPressed = 1; 
   });
-
-  // textX = width / 2;
-  // textY = height / 2;
 }
 
 function draw() {
   if (buttonPressed === 1) {
-    newPage(); // Switch to eerie text page
+    newPage(); 
   } else {
-    mainPage(); // Normal flashlight interaction
+    mainPage(); 
   }
 }
 
 function mainPage() {
   background(0);
 
-  // Flashlight effect
+  // Flashlight effect (scale based on window size)
   fill(255, 255, 255, 150);
   noStroke();
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, windowWidth * 0.08, windowWidth * 0.08); // Use windowWidth for flashlight size
 
-  // Eye positions
+  // Eye positions (scaled)
   let eyes = [
-    { x: 70, y: 70, length: 95 },
-    { x: 280, y: 280, length: 100 },
-    { x: 420, y: 170, length: 110 }
+    { x: windowWidth * 0.1, y: windowHeight * 0.1, length: windowWidth * 0.1 },
+    { x: windowWidth * 0.3, y: windowHeight * 0.5, length: windowWidth * 0.12 },
+    { x: windowWidth * 0.45, y: windowHeight * 0.3, length: windowWidth * 0.13 }
   ];
 
   let showButton = 0; // Track if near red-pupil eye
@@ -48,21 +45,21 @@ function mainPage() {
     let eye = eyes[i];
     let d = dist(mouseX, mouseY, eye.x, eye.y);
     
-    if (d < 60) { // Only show if within range
+    if (d < windowWidth * 0.07) { // Scale the proximity range
       stroke(255);
       line(eye.x - eye.length / 2, eye.y, eye.x + eye.length / 2, eye.y);
 
-      if (eye.x === 70) {
-        line(60, 70, 100, 70);
-        line(90, 70, 120, 70);
-      } else if (eye.x === 280) { // Red-pupil eye
-        line(280, 280, 280 + 50, 280);
+      if (eye.x === windowWidth * 0.1) {
+        line(eye.x - 30, eye.y, eye.x + 30, eye.y);
+        line(eye.x + 20, eye.y, eye.x + 50, eye.y);
+      } else if (eye.x === windowWidth * 0.3) { // Red-pupil eye
+        line(eye.x, eye.y, eye.x + 50, eye.y);
         stroke(255, 0, 0);
-        point(280, 280);
+        point(eye.x, eye.y);
         showButton = 1; // Show button when near this eye
-      } else if (eye.x === 420) {
-        line(420, 155, 420 + 45, 155);
-        line(420, 185, 420 + 45, 185);
+      } else if (eye.x === windowWidth * 0.45) {
+        line(eye.x, eye.y - 15, eye.x + 45, eye.y - 15);
+        line(eye.x, eye.y + 15, eye.x + 45, eye.y + 15);
       }
     }
   }
@@ -76,30 +73,7 @@ function mainPage() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+
+  // Reposition the button relative to new window size
+  button.position(windowWidth * 0.4, windowHeight * 0.4);
 }
-
-// function newPage() {
-//   background(0);
-//   button.hide();
-
-//   // Shaky text effect
-//   let jitterX = random(-1.5, 1.5);
-//   let jitterY = random(-1.5, 1.5);
-
-//   fill(255, 255, 255);
-//   stroke(0);
-//   textSize(20);
-//   textAlign(CENTER, TOP);
- 
-//   // textFont(fontBold);
-//   text('Do me favour, you have to keep on. Whatever it is, DO NOT turn it off', textX + jitterX, textY + jitterY);
-
-  // "false" optionA
-  // button = createButton('Okay');
-  // button.position(130, 320);
-
-  // // optionB
-  // button = createButton('Not yet');
-  // button.position(200, 320);
-
-
